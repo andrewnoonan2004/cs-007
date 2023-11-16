5a114af57c443472574b374688c597131466d05a
Andrew Noonan <andrewnoonan@mailbox.org>
2023-11-15 19:57:29 -0500

    making some progress

--- Project7/jumbles.java
+++ Project7/jumbles.java
@@ -1,24 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-public class jumbles
-{
-	public static void main( String args[]) throws Exception 
+public class Jumbles {
+    public static void main(String args[]) throws Exception {
+        if (args.length < 2) {
+            System.out.println("\nusage:java jumbles <dictionary file> <jumbled file>\n");
+            System.exit(0);
+        }
+        BufferedReader dict = new BufferedReader(new FileReader(args[0]));
+        
+        ArrayList<String> dictList = new ArrayList<String>();
+        while (dict.ready()) {
+            dictList.add(dict.readLine());
+        }
+
+        Collections.sort(dictList);
+
+        ArrayList<String> pairs = new ArrayList<String>();
+	for (String regWord: dictList)
 	{
-		if (args.length<2)
-		{
-			System.out.println("\nusage:java jumbles <dictionary file> <jumbled file>\n");
-			System.exit(0);
-		}
-		BufferedReader dict = new BufferedReader( new FileReader(args[0]) );
-		while(dict.ready())
-		{
-			ArrayList<String> dictList = new ArrayList<String>();
-			dictList.add(dict.readLine());
-		};
-		Collections.sort(dictList());
-			ArrayList<String> pairs = new ArrayList<String>();
-			
-			System.out.println(dictList);
-	}
+		String canonWord = Canonical(regWord);
+		String pair = canonWord + " " + regWord;
+		pairs.add(pair);
+
+    }
+    Collections.sort(pairs);
+    for (String words : pairs)
+		System.out.println(words);
 }
+private static String Canonical(String word) {
+	char[] chars = word.toCharArray();
+	Arrays.sort(chars);
+	return new String(chars);
+}
+}
+
